d42c6ca7224d14bdf32ba6a111fa808ec53f8a2b
achinnasamy <chinnasamyad@gmail.com>
2019-01-30 12:18:38 +0530

    files

---
+++ src/com/dmac/scala_programming/TraitAsEnum.scala
@@ -0,0 +1,26 @@
+package com.dmac.scala_programming
+
+
+
+object TraitAsEnum {
+
+  def main(args: Array[String]) = {
+
+
+
+    import sqladvanced.PACKAGE_OBJECT._
+
+    if ("XmlFile".equals(^(XMLFILE)))
+      println (^(XMLFILE))
+
+
+    functionWithOptionParameter("satoshi", 1000, Some("block"))
+
+
+  }
+
+}
+
+abstract class ContractFactory(name : String) {
+
+}
