08af5f7c9788e68393b838e10aea36e684b1773a
achinnasamy <chinnasamyad@gmail.com>
2019-01-30 12:15:15 +0530

    files

---
+++ src/com/dmac/atos/test/BestPracticesTest.scala
@@ -0,0 +1,11 @@
+package com.dmac.atos.test
+
+import com.dmac.atos.BestPractice
+
+object BestPracticesTest {
+
+  def main(args: Array[String]): Unit = {
+    val bp = new BestPractice("DATA")
+    bp UsingtheTry()
+  }
+}
